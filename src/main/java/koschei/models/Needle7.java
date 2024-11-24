1baacbf594775f93ed9a2f635aeed29d719c673b
Pavel Ripanov <shurf.pc@gmail.com>
2024-11-24 18:57:27 +0300

    Test pass

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,7 +1,16 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Needle7 {
+    private Deth8 deth;
+
+    @Autowired
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
