18810e6305b1a9ab9e386dc2399e33e437b091ea
Pavel Ripanov <shurf.pc@gmail.com>
2024-11-27 20:42:24 +0300

    Base file format

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -11,8 +11,9 @@ public class Needle7 {
     public Needle7(Deth8 deth) {
         this.deth = deth;
     }
+
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( ";
     }
 }
